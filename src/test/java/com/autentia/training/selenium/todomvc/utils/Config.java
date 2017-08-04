d5e24058bc4698607dabb0b9902206fb7d3f7025
calberca <cesalberca@gmail.com>
2017-08-04 16:42:05 +0200

    Modified base url

--- src/test/java/com/autentia/training/selenium/todomvc/utils/Config.java
+++ src/test/java/com/autentia/training/selenium/todomvc/utils/Config.java
@@ -1,5 +1,5 @@
 package com.autentia.training.selenium.todomvc.utils;
 
 final class Config {
-    static final String BASE_URL = System.getProperty("baseUrl", "http://127.0.0.1:3000");
+    static final String BASE_URL = System.getProperty("baseUrl", "http://192.168.99.100:49002/");
 }
