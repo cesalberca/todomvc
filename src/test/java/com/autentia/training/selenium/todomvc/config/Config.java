4f8a305f9af5f4cc3a231873f3d860e6a7241324
calberca <cesalberca@gmail.com>
2017-10-09 14:17:33 +0200

    Change back port

--- src/test/java/com/autentia/training/selenium/todomvc/config/Config.java
+++ src/test/java/com/autentia/training/selenium/todomvc/config/Config.java
@@ -2,7 +2,7 @@ package com.autentia.training.selenium.todomvc.config;
 
 public final class Config {
 
-    public static final String HUB_URL = System.getProperty("hub.url", "http://selenium-hub:49002/wd/hub");
+    public static final String HUB_URL = System.getProperty("hub.url", "http://selenium-hub:4444/wd/hub");
     public static final String BASE_URL = System.getProperty("base.url", "http://todomvc.com/examples/vanillajs/");
     public static final String ENV = System.getProperty("env", "dev");
     public static final boolean DEBUG = Boolean.valueOf(System.getProperty("debug", "false"));
