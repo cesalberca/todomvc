7268c4104f8987c929a8024a667e1456be256aa3
César Alberca <cesalberca@gmail.com>
2017-10-11 18:35:03 +0200

    Add support for more platforms

--- src/test/java/com/autentia/training/selenium/todomvc/drivers/DriverResourceLocator.java
+++ src/test/java/com/autentia/training/selenium/todomvc/drivers/DriverResourceLocator.java
@@ -1,14 +1,31 @@
 package com.autentia.training.selenium.todomvc.drivers;
 
+import org.apache.commons.lang3.SystemUtils;
+
 import java.io.File;
 import java.net.URL;
 
 class DriverResourceLocator {
 
-    private static final String CHROME_DRIVER_NAME = "chromedriver-mac";
+    private static final String CHROME_DRIVER_NAME = "chromedriver";
+    private static final String MAC_SUFFIX = "mac";
+    private static final String WINDOWS_SUFFIX = "win";
+    private static final String LINUX_SUFFIX = "lin";
 
     String getChromeDriverLocation() {
-        return getDriverFromResources(CHROME_DRIVER_NAME);
+        return getDriverFromResources(getPlatformDriver());
+    }
+
+    private String getPlatformDriver() {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            return CHROME_DRIVER_NAME + "-" + WINDOWS_SUFFIX + ".exe";
+        } else if (SystemUtils.IS_OS_MAC) {
+            return CHROME_DRIVER_NAME + "-" + MAC_SUFFIX;
+        } else if (SystemUtils.IS_OS_LINUX) {
+            return CHROME_DRIVER_NAME + "-" + LINUX_SUFFIX;
+        } else {
+            throw new RuntimeException("Unsupported platform");
+        }
     }
 
     private String getDriverFromResources(String driverName) {
